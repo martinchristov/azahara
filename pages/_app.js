2b4e06d2c918d6bd019db8ac87e809c5a4ab7271
Martin Christov <martin.christov@gmail.com>
2023-04-21 08:31:18 +0200

    basic routing

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,20 @@
 import '@/styles/globals.scss'
+import Image from 'next/image'
+
+const Layout = ({ children }) => {
+  return (
+    <main className="min-h-screen">
+      <Image className="mobile-only bg" src="/bg.jpg" alt="bg" fill />
+      <Image className="desktop-only bg" src="/bg-desktop.jpg" alt="bg" fill />
+      {children}
+    </main>
+  )
+}
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+  <Layout>
+    <Component {...pageProps} />
+  </Layout>
+  )
 }
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import { useEffect, useState } from 'react'
 import moment from 'moment'
+import Link from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,32 +17,30 @@ export default function Home() {
     })
   }, [])
   return (
-    <main className="min-h-screen">
-      <Image className="mobile-only bg" src="/bg.jpg" alt="bg" fill />
-      <Image className="desktop-only bg" src="/bg-desktop.jpg" alt="bg" fill />
-      <div className="container mx-auto">
-        <div className="retreats">
-        {retreats.map(retreat => {
-          const { Title, Subtitle, Starts, Ends, Cover } = retreat.attributes
-          return (
-            <div className="retreat bg-whitepx-4 sm:px-6 sm:pt-8 rounded-lg m-10" key={retreat.id}>
-              <div className="thumb col">
-                {Cover.data != null &&
-                <Image src={Cover.data.attributes.formats.small.url} fill alt="cover" />
-                }
-              </div>
-              <div className="col">
-                <div className='flex'>
-                  <div className="date rounded-sm">{moment(Starts, 'YYYY-MM-DD').format('MMM DD')} - {moment(Ends, 'YYYY-MM-DD').format('MMM DD')}</div>
-                </div>
-                <h2>{Title}</h2>
-                <h3>{Subtitle}</h3>
-              </div>
+    <div className="container mx-auto">
+      <div className="retreats">
+      {retreats.map(retreat => {
+        const { Title, Subtitle, Starts, Ends, Cover, Slug } = retreat.attributes
+        return (
+          <Link href={`/retreats/${Slug}`} key={retreat.id}>
+          <div className="retreat bg-whitepx-4 sm:px-6 sm:pt-8 rounded-lg m-6">
+            <div className="thumb col">
+              {Cover.data != null &&
+              <Image src={Cover.data.attributes.formats.small.url} fill alt="cover" />
+              }
             </div>
-          )
-        })}
-        </div>
+            <div className="col">
+              <div className='flex'>
+                <div className="date rounded-sm">{moment(Starts, 'YYYY-MM-DD').format('MMM DD')} - {moment(Ends, 'YYYY-MM-DD').format('MMM DD')}</div>
+              </div>
+              <h2>{Title}</h2>
+              <h3>{Subtitle}</h3>
+            </div>
+          </div>
+          </Link>
+        )
+      })}
       </div>
-    </main>
+    </div>
   )
 }
