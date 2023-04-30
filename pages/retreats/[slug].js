90b03c4ccf9b08b3297649ec90a68c1c64643733
Martin Christov <martin.christov@gmail.com>
2023-04-30 11:52:03 +0300

    datepicker bugfix

--- pages/retreats/[slug].js
+++ pages/retreats/[slug].js
@@ -107,11 +107,11 @@ const FullView = ({ data }) => {
               <div className="flex grid grid-flow-col auto-rows-max gap-x-1.5">
                 <div className="flex flex-col">
                   <div className="label">Check in</div>
-                  <DatePicker size="large" inputReadOnly value={dayjs(booking.checkIn, 'YYYY-MM-DD')} disabledDate={(date) => date.valueOf() < now.valueOf() || date.valueOf() > dayjs(booking.checkOut, 'YYYY-MM-DD')} format="DD MMM" onChange={handleUpdateBooking('checkIn')} />
+                  <DatePicker size="large" allowClear={false} inputReadOnly value={dayjs(booking.checkIn, 'YYYY-MM-DD')} disabledDate={(date) => date.valueOf() < now.valueOf() || date.valueOf() > dayjs(booking.checkOut, 'YYYY-MM-DD')} format="DD MMM" onChange={handleUpdateBooking('checkIn')} />
                 </div>
                 <div className="flex flex-col">
                   <div className="label">Check out</div>
-                  <DatePicker size="large" inputReadOnly value={dayjs(booking.checkOut, 'YYYY-MM-DD')} disabledDate={(date) => date.valueOf() < dayjs(booking.checkIn, 'YYYY-MM-DD')} format="DD MMM" onChange={handleUpdateBooking('checkOut')} />
+                  <DatePicker size="large" allowClear={false} inputReadOnly value={dayjs(booking.checkOut, 'YYYY-MM-DD')} disabledDate={(date) => date.valueOf() < dayjs(booking.checkIn, 'YYYY-MM-DD')} format="DD MMM" onChange={handleUpdateBooking('checkOut')} />
                 </div>
               </div>
               <div className="flex grid grid-flow-col auto-rows-max gap-x-1.5 mt-3">
@@ -300,7 +300,10 @@ const CheckoutView = ({ setStep, data, booking }) => {
           </ul>
         </div>
         <div className="payment">
-          <h4>Payment details</h4>
+          <div className="flex mt-2 mb-4">
+            <h4>Payment details</h4>
+            {/* <Button type="link" size="small" className="ml-auto">Cancellation policy</Button> */}
+          </div>
           <Form
             // {...formItemLayout}
             layout="vertical"
