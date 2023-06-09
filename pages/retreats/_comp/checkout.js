d59bb90c4a02fb902c374e08c6e63078566cd121
Martin Christov <martin.christov@gmail.com>
2023-06-09 10:07:09 +0300

    checkout page update

--- pages/retreats/_comp/checkout.js
+++ pages/retreats/_comp/checkout.js
@@ -59,60 +59,69 @@ const CheckoutView = ({
         <RetreatHeader data={data} />
         <hr />
         <h4>Booking Details</h4>
-        <div className="details">
-          <ul>
-            <li>
-              <span>Dates</span>
-              <strong>
-                {dayjs(booking.checkIn).format('DD MMM')} -{' '}
-                {dayjs(booking.checkOut).format('DD MMM')}
-              </strong>
-            </li>
-            <li>
-              <span>Guests</span>
-              <strong>
-                {booking.adults} {booking.adults === 1 ? 'adult' : 'adults'},{' '}
-                {booking.children === 0
-                  ? 'no children'
-                  : `${booking.children} children`}
-              </strong>
-            </li>
-            {booking.room && (
-              <>
-                <li>
-                  <span>Accommodation</span>
-                  <strong>
-                    {dayjs(booking.checkOut).diff(booking.checkIn, 'days')}
-                    {' days in '}
-                    {booking.room.title}
-                  </strong>
-                </li>
-                <li>
-                  <span>Extras</span>
-                  <ul className="feats">
-                    <li>
-                      <Image src="/meal.svg" alt="bed" width={18} height={18} />{' '}
-                      Meals Included
-                    </li>
-                  </ul>
-                </li>
-                <li className="total">
-                  <span>Total amount due</span>
-                  <strong>{calcPrice(booking.room, booking, retreat)}</strong>
-                </li>
-                <li className="total deposit">
-                  <span>50% Deposit Due Now</span>
-                  <strong>
-                    {calcPrice(booking.room, booking, retreat, true)}
-                  </strong>
-                </li>
-                <li>
-                  <small>The remainder 50% is payable in cash on arrival</small>
-                </li>
-              </>
-            )}
-          </ul>
-        </div>
+        {booking && (
+          <div className="details">
+            <ul>
+              <li>
+                <span>Dates</span>
+                <strong>
+                  {dayjs(booking.checkIn).format('DD MMM')} -{' '}
+                  {dayjs(booking.checkOut).format('DD MMM')}
+                </strong>
+              </li>
+              <li>
+                <span>Guests</span>
+                <strong>
+                  {booking.adults} {booking.adults === 1 ? 'adult' : 'adults'},{' '}
+                  {booking.children === 0
+                    ? 'no children'
+                    : `${booking.children} children`}
+                </strong>
+              </li>
+              {booking.room && (
+                <>
+                  <li>
+                    <span>Accommodation</span>
+                    <strong>
+                      {dayjs(booking.checkOut).diff(booking.checkIn, 'days')}
+                      {' days in '}
+                      {booking.room.title}
+                    </strong>
+                  </li>
+                  <li>
+                    <span>Extras</span>
+                    <ul className="feats">
+                      <li>
+                        <Image
+                          src="/meal.svg"
+                          alt="bed"
+                          width={18}
+                          height={18}
+                        />{' '}
+                        Meals Included
+                      </li>
+                    </ul>
+                  </li>
+                  <li className="total">
+                    <span>Total amount due</span>
+                    <strong>{calcPrice(booking.room, booking, retreat)}</strong>
+                  </li>
+                  <li className="total deposit">
+                    <span>50% Deposit Due Now</span>
+                    <strong>
+                      {calcPrice(booking.room, booking, retreat, true)}
+                    </strong>
+                  </li>
+                  <li>
+                    <small>
+                      The remainder 50% is payable in cash on arrival
+                    </small>
+                  </li>
+                </>
+              )}
+            </ul>
+          </div>
+        )}
         <div className="payment">
           <div className="flex mt-2 mb-4">
             <h4>Payment details</h4>
@@ -123,34 +132,6 @@ const CheckoutView = ({
               <CheckoutForm />
             </Elements>
           )}
-          {/* <Form
-            // {...formItemLayout}
-            layout="vertical"
-            form={form}
-            onValuesChange={() => {}}
-          >
-            <Form.Item name="email">
-              <Input size="large" placeholder="Email" />
-            </Form.Item>
-            <Form.Item name="name">
-              <Input size="large" placeholder="Cardholder Name" />
-            </Form.Item>
-            <Form.Item name="card">
-              <Input size="large" placeholder="Card Number" />
-            </Form.Item>
-            <div className="grid grid-flow-col auto-rows-max gap-x-1.5">
-              <Form.Item name="expDate">
-                <Input size="large" placeholder="Expiration Date" />
-              </Form.Item>
-              <Form.Item name="card">
-                <Input size="large" placeholder="CVV" />
-              </Form.Item>
-            </div>
-          </Form>
-          <div className="stripe">Secure checkout with Stripe</div>
-          <Button type="primary" size="large">
-            Make Booking
-          </Button> */}
         </div>
       </div>
     </div>
@@ -233,16 +214,28 @@ const CheckoutForm = () => {
 
   return (
     <form id="payment-form" onSubmit={handleSubmit}>
-      <LinkAuthenticationElement
+      <label for="name-input">Name</label>
+      {/* <input name="name" id="name-input" /> */}
+      {/* <LinkAuthenticationElement
         id="link-authentication-element"
-        onChange={(e) => setEmail(e.target.value)}
-      />
+        onChange={(e) => {
+          console.log(e)
+          setEmail(e.value.email)
+        }}
+      /> */}
       <PaymentElement id="payment-element" options={paymentElementOptions} />
-      <button disabled={isLoading || !stripe || !elements} id="submit">
+      {/* <button id="submit"></button> */}
+      <Button
+        disabled={isLoading || !stripe || !elements}
+        type="primary"
+        id="submit"
+        size="large"
+        style={{ marginTop: 20 }}
+      >
         <span id="button-text">
           {isLoading ? <div className="spinner" id="spinner"></div> : 'Pay now'}
         </span>
-      </button>
+      </Button>
       {/* Show any error or success messages */}
       {message && <div id="payment-message">{message}</div>}
     </form>
